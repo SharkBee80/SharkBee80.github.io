397a0f7a327180e3d7cc10bf3414f40214a7577f
sharkbee <woo321a@outlook.com>
2025-08-15 01:07:10 +0800

    refactor(home): 优化首页滚动逻辑
    - 新增 home.js 文件，用于处理首页滚动事件
    - 修改 404.html、about.html、archives.html、categories.html 中的导航链接，指向对应的 demo 页面
    - 更新 home/demo.html 和 home/dev.html，添加新首页结构
    - 调整 assets/css/main.css 中的样式，注释掉部分代码
    - 优化 assets/js/events.js 中的事件处理函数

---
+++ home/home.js
@@ -0,0 +1,10 @@
+document.addEventListener('wheel', function (event) {
+    const scroll_down_bar = jQuery('.scroll-down-bar');
+    if (scroll_down_bar.length !== 1 || scroll_down_bar[0].offsetHeight <= 0) {
+        return;
+    }
+    if (event.deltaY > 0) {
+        scroll_down_bar.click();
+        document.removeEventListener('wheel', arguments.callee);
+    }
+});
